df5c38d3ee7d18c0408c76477aa7c4c1d63ad732
Jidraph! <jidraphkimachia1@gmail.com>
2023-07-26 16:24:32 +0300

    Node modules and JWT auth practice

---
+++ index.js
@@ -0,0 +1,6 @@
+
+const Person = require ('./person')
+
+const person1 = new Person('John', 23)
+
+console.log(person1.name, person1.greeting())
---
+++ reference/path_demo.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+// Base file name
+// console.log(path.basename(__filename))
+// This gives you the current filename path
+
+// Directory Name
+// console.log(path.dirname(__filename))
+// returns the Directory Name
+
+// FileEXtension
+// console.log(path.extname(__filename))
+// returns the extension name in this case, JS
+
+// create path Object
+// console.log(path.parse(__filename))
+// returns the rootCertificates, directory, base, extensiin and directory name
